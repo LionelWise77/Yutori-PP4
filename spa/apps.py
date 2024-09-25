326ce2dc2c343f5dc313b19b5730de7dcd7d5b56
Sebastian Perez Betancur <Sebbepebe27@gmail.com>
2024-09-25 11:02:41 +0000

    create file signals to save and create clients

--- spa/apps.py
+++ spa/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class SpaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'spa'
+
+def ready(self):
+        import spa.signals
