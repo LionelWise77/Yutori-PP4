eea24a200d3d9fe57fb35c5f225d5f4be6b7f069
Sebastian Perez Betancur <Sebbepebe27@gmail.com>
2025-01-11 13:50:37 +0000

    Enhance security by moving sensitive information to environment variables
    - Moved SECRET_KEY to environment variables
    - Set DEBUG based on environment variable
    - Updated settings.py to load environment variables from .env file
    - Added .env file to .gitignore to prevent it from being tracked by version control

--- yutori/settings.py
+++ yutori/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9v*1m-we=#lhtpzex%@6vlx7hf8u5xix@!$ix#jz#lk$!5w8_s'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ["8000-lionelwise77-yutoripp4-51lotwowxtv.ws.codeinstitute-ide.net",
 ".herokuapp.com"
