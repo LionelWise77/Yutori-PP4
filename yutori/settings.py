2bb38dcecb981aee82215a680ee9fc0a7c253264
LioneWise77 <sebbepebe27@gmail.com>
2025-06-08 10:46:16 +0200

    Enable debug temporarily to diagnose 500 error

--- yutori/settings.py
+++ yutori/settings.py
@@ -24,10 +24,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = [
